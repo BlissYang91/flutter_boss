aada2a2957fe648dc71be1c4ef2b5c5e73378c89
yangtianfu <2812420513@qq.com>
2022-03-13 06:15:00 +0800

    data done

--- lib/view/jobs_view.dart
+++ lib/view/jobs_view.dart
@@ -21,8 +21,75 @@ class JobList extends State<JobsTab> {
   void getJobList() {
     setState(() {
       _jobs = Job.fromJson("""
-        {"list":[{"name":"架构师（Android）","cname":"蚂蚁金服","size":"B轮","salary":"25K-45K","username":"Claire","title":"HR"},
-{"name":"资深Android架构师","cname":"今日头条","size":"D轮","salary":"40K-60K","username":"Kimi","title":"HRBP"}]}""");
+        {
+          "list":[
+              {
+                  "name":"架构师（Android）",
+                  "cname":"蚂蚁金服",
+                  "size":"B轮",
+                  "salary":"25K-45K",
+                  "username":"Claire",
+                  "title":"HR"
+              },
+              {
+                  "name":"资深Android架构师",
+                  "cname":"今日头条",
+                  "size":"D轮",
+                  "salary":"40K-60K",
+                  "username":"Kimi",
+                  "title":"HRBP"
+              },
+              {
+                  "name":"高级Android工程师（Framework方向）",
+                  "cname":"爱奇艺",
+                  "size":"已上市",
+                  "salary":"35K-65K",
+                  "username":"Lecns",
+                  "title":"HR"
+              },
+              {
+                  "name":"Flutter开发工程师",
+                  "cname":"今日头条",
+                  "size":"D轮",
+                  "salary":"30K-40K",
+                  "username":"Davied",
+                  "title":"HRBP"
+              },
+              {
+                  "name":"高级java开发",
+                  "cname":"百度",
+                  "size":"B轮",
+                  "salary":"35K-45K",
+                  "username":"Claire",
+                  "title":"HR"
+              },
+              {
+                  "name":"web前端开发工程师",
+                  "cname":"bilibili",
+                  "size":"D轮",
+                  "salary":"40K-60K",
+                  "username":"hua",
+                  "title":"HRBP"
+              },
+              {
+                  "name":"IOS高级开发工程师",
+                  "cname":"阿里巴巴",
+                  "size":"D轮",
+                  "salary":"35K-45K",
+                  "username":"Alin",
+                  "title":"HR"
+              },
+              {
+                  "name":"项目管理",
+                  "cname":"字节跳动",
+                  "size":"D轮",
+                  "salary":"40K-60K",
+                  "username":"code",
+                  "title":"项目经理"
+              }
+          ]
+        }
+        """);
     });
   }
 
